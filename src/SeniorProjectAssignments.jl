25350a5154fe1db2c848c491a05a7a454293fd60
Zachary Sunberg <zachary.sunberg@colorado.edu>
2020-08-17 12:57:07 -0600

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.8

---
+++ src/SeniorProjectAssignments.jl
@@ -0,0 +1,5 @@
+module SeniorProjectAssignments
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SeniorProjectAssignments
+using Test
+
+@testset "SeniorProjectAssignments.jl" begin
+    # Write your tests here.
+end
